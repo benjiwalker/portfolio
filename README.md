7447892e88a76752b5dcf7aab4cf84e1078fda25
neiter-neither <ben.walker.1212.dev@gmail.com>
2023-02-21 22:09:56 +0000

    added more projects

--- README.md
+++
@@ -1,3 +0,0 @@
-# Benjamin Walker Portfolio
-
-You can download my CV [here](https://github.com/benjiwalker/portfolio/blob/main/CV.pdf)
---
+++ src/lib/data/projects.ts
@@ -0,0 +1,48 @@
+const projects = [
+	{
+		title: 'Ormside Projects',
+		link: 'https://ormside.co.uk',
+		github: 'https://github.com/benjiwalker/ormside-projects',
+		text: 'I designed and built the website for arts and event space Ormside Projects. ',
+		tech: ['Gatsby', 'Typescript', 'GraphQL', 'Node.js', 'Contentful', 'Netlify']
+	},
+	{
+		title: 'Susu Laroche',
+		link: 'https://susularoche.com',
+		github: 'https://github.com/benjiwalker/susu-laroche',
+		text: 'I designed and built the website for multi-displiciary artist Susu Laroche. ',
+		tech: ['Gatsby', 'Typescript', 'GraphQL', 'Node.js', 'Contentful', 'Netlify']
+	},
+	{
+		title: 'Relaxin Records',
+		link: 'https://relaxinrecords.com',
+		github: '',
+		text: 'I designed and built the website for Lolina`s new record label, Relaxin Records. ',
+		tech: ['Gatsby', 'Typescript', 'GraphQL', 'Node.js', 'Contentful', 'Netlify']
+	},
+	{
+		title: 'CYLNDR',
+		link: '',
+		github: '',
+		text: 'I designed a web app for advertising agency CYLNDR. The web app linked the API’s of the different workflow services, such as Google Sheets, Ganttic and Dropbox, to combine separate tasks into one task.',
+		tech: ['React', 'Javascript', 'Material UI', 'Node.js', 'Firebase']
+	},
+	{
+		title: 'Dub Siren',
+		github: 'https://github.com/benjiwalker/dub-siren',
+		link: 'https://dubsiren.web.app/',
+		text: 'A broswer based version of the classic Dub Siren.',
+
+		tech: ['React', 'Typescript', 'Tone.js']
+	},
+	{
+		title: 'Disclist',
+		github: 'https://github.com/benjiwalker/disclist',
+		link: 'https://disclist.netlify.app/',
+		text: 'A web app that displays all the YouTube videos linked to an artist page on the vinyl marketplace website Discogs. The app is designed to create a beter user experience for listening to an artists full discography.',
+
+		tech: ['Svelte', 'Typescript', 'Node.js', 'Netlify']
+	}
+];
+
+export default projects;
